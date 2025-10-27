5b4d30cc6ecc6103d0ce2f2ba11841ddc78d5a26
Mohamed Ali BELHADJ <mohamed@mozark.ai>
2025-10-27 23:10:36 +0100

    Add project

---
+++ OolabMoviestest/OolabMoviestest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OolabMoviestest
+//
+//  Created by Mohamed Ali BELHADJ on 27/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OolabMoviestest/OolabMoviestest/OolabMoviestestApp.swift
@@ -0,0 +1,17 @@
+//
+//  OolabMoviestestApp.swift
+//  OolabMoviestest
+//
+//  Created by Mohamed Ali BELHADJ on 27/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct OolabMoviestestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
